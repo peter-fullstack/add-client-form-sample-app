dacd4c1b7be9638d7695b61bb281b09bb951d9db
peter-fullstack <peter.fullstack@gmail.com>
2023-09-17 11:20:09 +1200

    Implemented save spinner on button

--- react-redux-spa/src/components/AddClient.tsx
+++ react-redux-spa/src/components/AddClient.tsx
@@ -1,9 +1,11 @@
 import React, { FormEvent, useState } from "react";
 import CompanyModel from "../models/CompanyModel";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createCompany } from "../slices/companiesSlice";
 import { AnyAction } from "@reduxjs/toolkit";
 import { AppDispatch } from "../store";
+import { StateModel } from "../models/SateModel";
+import { fetchStatus } from "../models/FetchStatus";
 // import { useDispatch } from "react-redux";
 //import { createTutorial } from "../slices/clients";
 
@@ -15,6 +17,11 @@ export const AddClient = () => {
         city: "",
         state: "",
     };
+
+    const state = useSelector((state: any) => {
+        return state.companies;
+    });
+
     const [company, setCompany] = useState(initialCompanyState);
     const [submitted, setSubmitted] = useState(false);
 
@@ -28,23 +35,23 @@ export const AddClient = () => {
     const saveTutorial = () => {
         const { companyName, address, city, state } = company;
 
-        dispatch(createCompany(company))
+        dispatch(createCompany(company));
 
-    //         .unwrap()
-    //         .then((data: CompanyModel) => {
-    //             console.log(data);
-    //             setCompany({
-    //                 id: data.id,
-    //                 address: data.address,
-    //                 companyName: data.companyName,
-    //                 city: data.city,
-    //                 state: data.state})
-    //             setSubmitted(true);
-    //         })
-    //         .catch(e => {
-    //             console.log(e);
-    //         });
-    // 
+        //         .unwrap()
+        //         .then((data: CompanyModel) => {
+        //             console.log(data);
+        //             setCompany({
+        //                 id: data.id,
+        //                 address: data.address,
+        //                 companyName: data.companyName,
+        //                 city: data.city,
+        //                 state: data.state})
+        //             setSubmitted(true);
+        //         })
+        //         .catch(e => {
+        //             console.log(e);
+        //         });
+        // 
     };
 
     const newTutorial = () => {
@@ -54,73 +61,65 @@ export const AddClient = () => {
 
     return (
         <div className="submit-form">
-            {submitted ? (
-                <div>
-                    <h4>You submitted successfully!</h4>
-                    <button className="btn btn-success" onClick={newTutorial}>
-                        Add
-                    </button>
-                </div>
-            ) : (
-                <div>
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Company</h5>
-                            <div className="form-group">
-                                <label htmlFor="companyName">Company Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="companyName"
-                                    required
-                                    value={company.companyName || ''}
-                                    onChange={handleInputChange}
-                                    name="companyName"
-                                />
-                            </div>
+            <div>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Company</h5>
+                        <div className="form-group">
+                            <label htmlFor="companyName">Company Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="companyName"
+                                required
+                                value={company.companyName || ''}
+                                onChange={handleInputChange}
+                                name="companyName"
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="address">Address</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="address"
-                                    required
-                                    value={company.address || ''}
-                                    onChange={handleInputChange}
-                                    name="address"
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="address">Address</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="address"
+                                required
+                                value={company.address || ''}
+                                onChange={handleInputChange}
+                                name="address"
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="city">City</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="city"
-                                    required
-                                    value={company.city || ''}
-                                    onChange={handleInputChange}
-                                    name="city"
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="city">City</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="city"
+                                required
+                                value={company.city || ''}
+                                onChange={handleInputChange}
+                                name="city"
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label htmlFor="state">State</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="state"
-                                    required
-                                    value={company.state || ''}
-                                    onChange={handleInputChange}
-                                    name="state"
-                                />
-                            </div>
+                        <div className="form-group">
+                            <label htmlFor="state">State</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="state"
+                                required
+                                value={company.state || ''}
+                                onChange={handleInputChange}
+                                name="state"
+                            />
                         </div>
                     </div>
+                </div>
 
-                    {/* <div className="card">
+                {/* <div className="card">
                         <div className="card-body">
                             <h5 className="card-title">Contact</h5>
                             <div className="form-group">
@@ -181,11 +180,20 @@ export const AddClient = () => {
                         </div>
                     </div> */}
 
-                    <button onClick={saveTutorial} className="btn btn-success">
-                        Save
-                    </button>
-                </div>
-            )}
+                <button onClick={saveTutorial} className="btn btn-success" style={{width: "200px"}}>
+                    {state.loadingStatus === fetchStatus.loading ? (
+                        <div>
+                            <span className="spinner-border spinner-border-sm">
+                            </span>
+                            <span style={{paddingLeft: "5px"}}>
+                                Saving ...
+                            </span>
+                        </div>
+                    ) : (
+                        <span>Save</span>
+                    )}
+                </button>
+            </div>
         </div>
     );
 };
